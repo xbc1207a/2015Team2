e157d4aaffe1aa8ed058014101a76c31a50a0c02
Wlogsky <ss840429@gmail.com>
2015-12-15 20:46:12 +0800

    Goods structure

---
+++ TeamProject/Goods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    class Goods
+    {
+        public Goods() {
+            Clear();
+        }
+
+        public string name   { get { return name_ ;  } }
+        public    int price  { get { return price_;  } }
+        public    int stock  { get { return stock_;  } }
+        public string detail { get { return detail_; } }
+        public string type   { get { return type_;   } }
+
+        public void SetName(string name) {
+            name_ = name;
+        }
+        public void SetPrice(int price) {
+            price_ = price;
+        }
+        public void SetStock(int stock) {
+            stock_ = stock;
+        }
+        public void SetDetail(string detail) {
+            detail_ = detail;
+        }
+        public void SetType(string type) {
+            type_ = type;
+        }
+        public void Clear() {
+            name_ = ""; price_ = 0; stock_ = 0; detail_ = ""; type_ = "";
+        }
+      
+  
+// ----------------------------------------------------------------------   Private
+
+        private string name_ ;
+        private int price_ ;
+        private int stock_ ;              // 庫存
+        private string detail_ ;          // 說明
+        private string type_ ;            // 類別
+    
+    }
+
+
+
+}
--- TeamProject/Tests/TestClass1.cs
+++ TeamProject/Tests/TestClass1.cs
@@ -11,9 +11,22 @@ namespace TeamProject.Tests
     public class TestClass1
     {
         [Test]
-        public void Test1()
+        public void TestGoods()
         {
-            Assert.That(1 + 1, Is.EqualTo(2));
+            Goods good = new Goods();
+            Assert.That(good.name, Is.EqualTo(""));
+            good.SetName("Apple");
+            good.SetPrice(30);
+            good.SetStock(100);
+            good.SetType("Fruit");
+            good.SetDetail("Just an apple");
+
+            Assert.That(good.name, Is.EqualTo("Apple"));
+            Assert.That(good.price, Is.EqualTo(30));
+            Assert.That(good.stock, Is.EqualTo(100));
+            Assert.That(good.type, Is.EqualTo("Fruit"));
+            Assert.That(good.detail, Is.EqualTo("Just an apple"));
+            
         }
     }
 }
