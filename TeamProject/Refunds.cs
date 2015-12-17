b1c8513840de39d1299391f6fbfff2b07e793a6d
40240306s <40240306s@ntnu.edu.tw>
2015-12-17 13:44:12 +0800

    退貨明細

--- TeamProject/Refunds.cs
+++ TeamProject/Refunds.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,41 @@ namespace TeamProject
 {
     class Refunds
     {
+        List<Goods> gdb = new List<Goods>();
+        public void Add(Goods g)
+        {
+            gdb.Add(g);
+        }
+
+        public void Add(Goods[] ga)
+        {
+            foreach (Goods g in ga)
+                Add(g);
+        }
+
+        public string Result()
+        {
+            int money = 0;
+            string s = "";
+            s += "退費明細\n" +
+                 "\n";
+            int count = 1;
+            s += string.Format("{0}\t:{1}\t:{2}\tx\t{3}\t=\t{4}\n", "項次", "品名", "價格", "數量", "金額");
+            foreach (Goods g in gdb)
+            {
+                int m = g.price * g.stock;
+                s += string.Format("{0}\t:{1}\t:{2}\tx\t{3}\t=\t{4}\n", count, g.name, g.price, g.stock, m);
+                ++count;
+                money += m;
+            }
+            s += "\n";
+            s += string.Format("小記:{0}", money);
+
+            return s;
+        }
+
+
+
+
     }
 }
--- TeamProject/Tests/TestClass1.cs
+++ TeamProject/Tests/TestClass1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TeamProject.Tests
 {
@@ -26,7 +27,7 @@ namespace TeamProject.Tests
             Assert.That(good.stock, Is.EqualTo(100));
             Assert.That(good.type, Is.EqualTo("Fruit"));
             Assert.That(good.detail, Is.EqualTo("Just an apple"));
-            
+
         }
         [Test]
         public void TestGoodsDB()
@@ -41,5 +42,51 @@ namespace TeamProject.Tests
             db.Clear();
             Assert.That(db.num, Is.EqualTo(0));
         }
+
+
+
+
+
+
+
+
+
+
+
+
+        Goods AGood(string name, int pri, int amount)
+        {
+            Goods g = new Goods();
+            g.SetName(name);
+            g.SetPrice(pri);
+            g.SetStock(amount);
+            return g;
+        }
+
+        [Test]
+        public void TestRefunds()
+        {
+            Refunds r = new Refunds();
+            Goods[] g = new Goods[] {
+                AGood("good1",100,1),
+                AGood("good2",50,2) };
+            r.Add(g);
+            Assert.That(r.Result(), Is.EqualTo("退費明細\n" + "\n" +
+            "項次	:品名	:價格	x	數量	=	金額\n" +
+            "1	:good1	:100	x	1	=	100\n" +
+            "2	:good2	:50	x	2	=	100\n" + "\n" +
+            "小記:200"));
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
